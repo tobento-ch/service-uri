877b1a891e38dd2ff0b64d130cf165c1c21a12a4
TOBENTOch <info@tobento.ch>
2023-06-10 09:16:12 +0200

    new tests added

--- tests/BaseUriTest.php
+++ tests/BaseUriTest.php
@@ -30,5 +30,63 @@ class BaseUriTest extends TestCase
         $uri = $factory->createUri('https://example.com/base/path/');
         
         $this->assertInstanceof(BaseUriInterface::class, new BaseUri($uri));
-    }    
+    }
+
+    public function testGetMethods()
+    {
+        $factory = new Psr17Factory();
+        $uri = $factory->createUri('https://user:pass@example.com:8080/base/path?q=abc#fragment');
+        $uri = new BaseUri($uri);
+        
+        $this->assertSame('https', $uri->getScheme());
+        $this->assertSame('user:pass@example.com:8080', $uri->getAuthority());
+        $this->assertSame('user:pass', $uri->getUserInfo());
+        $this->assertSame('example.com', $uri->getHost());
+        $this->assertSame(8080, $uri->getPort());
+        $this->assertSame('/base/path', $uri->getPath());
+        $this->assertSame('q=abc', $uri->getQuery());
+        $this->assertSame('fragment', $uri->getFragment());
+    }
+    
+    public function testWithMethods()
+    {
+        $factory = new Psr17Factory();
+        $uri = $factory->createUri('https://example.com/base/path/');
+        $uri = new BaseUri($uri);
+        
+        $this->assertSame(
+            'http://example.com/base/path/',
+            (string)$uri->withScheme('http')
+        );
+        
+        $this->assertSame(
+            'https://user:password@example.com/base/path/',
+            (string)$uri->withUserInfo('user', 'password')
+        );
+        
+        $this->assertSame(
+            'https://example.ch/base/path/',
+            (string)$uri->withHost('example.ch')
+        );
+        
+        $this->assertSame(
+            'https://example.com:8080/base/path/',
+            (string)$uri->withPort(8080)
+        );
+        
+        $this->assertSame(
+            'https://example.com/new',
+            (string)$uri->withPath('new')
+        );
+        
+        $this->assertSame(
+            'https://example.com/base/path/?q=abc',
+            (string)$uri->withQuery('q=abc')
+        );
+        
+        $this->assertSame(
+            'https://example.com/base/path/#fragment',
+            (string)$uri->withFragment('fragment')
+        );
+    }
 }
